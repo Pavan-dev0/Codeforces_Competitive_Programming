89255b4d606af6a833493d0345a598256de8bae0
Pavan-dev0 <altairpheonix@gmail.com>
2026-03-02 23:36:25 +0530

    word_capitalization problem code forces 281A

--- Practice/A_Level/way_too_long_words.py
+++ Practice/A_Level/way_too_long_words.py
@@ -3,8 +3,9 @@ for _ in range(n):
     letters=input().strip()
 
     lens=len(letters)
-    if lens<10:
+    if lens<=10:
         print(letters)
     else:
         print(letters[0]+str(lens-2)+letters[-1])
 
+1
